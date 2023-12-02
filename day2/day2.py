8963cb72aea5416b630837a53376a46a6a99a671
sadzikweronika <weronikasadzik13@gmail.com>
2023-12-02 11:04:02 +0100

    Added new solution

--- day2/day2.py
+++ day2/day2.py
@@ -1,5 +1,6 @@
 import aoc_lube
 from functools import reduce
+import re
 
 
 def preprocess_data(raw_data):
@@ -19,9 +20,9 @@ THRESHOLDS = {'red': 12, 'green': 13, 'blue': 14}
 
 def part1():
     total_score = 0
-    for game_index, game_data in enumerate(DATA):
+    for game_index, game_data in enumerate(DATA, start=1):
         if all(count <= THRESHOLDS[color] for count, color in game_data):
-            total_score += game_index + 1
+            total_score += game_index
     return total_score
 
 
@@ -33,6 +34,25 @@ def part2():
             max_values[color] = max(max_values[color], count)
         total += reduce(lambda x, y: x * y, max_values.values(), 1)
     return total
+
+
+def fancy_solution(data):
+    # source: https://www.reddit.com/r/adventofcode/comments/188w447/2023_day_2_solutions/
+    result1 = result2 = 0
+    for game_index, game_data in enumerate(data, start=1):
+        red_cubes = list(map(int, re.findall(r"(\d+) red", game_data)))
+        green_cubes = list(map(int, re.findall(r"(\d+) green", game_data)))
+        blue_cubes = list(map(int, re.findall(r"(\d+) blue", game_data)))
+        is_game = not any([
+            *filter(lambda x: x > 12, red_cubes),
+            *filter(lambda x: x > 13, green_cubes),
+            *filter(lambda x: x > 14, blue_cubes),
+        ])
+        if is_game:
+            result1 += game_index
+        result2 += max(red_cubes) * max(green_cubes) * max(blue_cubes)
+
+    return result1, result2
 
 
 if __name__ == "__main__":
