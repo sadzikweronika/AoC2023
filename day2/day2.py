4ff8706e2b5cf2f519e9f63adfb76e9e3221f859
sadzikweronika <weronikasadzik13@gmail.com>
2023-12-02 10:24:06 +0100

    Day2 optimized one more time

--- day2/day2.py
+++ day2/day2.py
@@ -1,32 +1,37 @@
 import aoc_lube
+from functools import reduce
 
-DATA = aoc_lube.fetch(year=2023, day=2).splitlines()
+
+def preprocess_data(raw_data):
+    processed_data = []
+    for line in raw_data:
+        games = line.split(':')[1].split(';')
+        game_data = [[int(count), color]
+                     for game in games
+                     for count, color in (color_data.split() for color_data in game.split(','))]
+        processed_data.append(game_data)
+    return processed_data
+
+
+DATA = preprocess_data(aoc_lube.fetch(year=2023, day=2).splitlines())
 THRESHOLDS = {'red': 12, 'green': 13, 'blue': 14}
 
 
 def part1():
     total_score = 0
     for game_index, game_data in enumerate(DATA):
-        games = game_data.split(':')[1].split(';')
-        if all(int(count) <= THRESHOLDS[color]
-               for game in games
-               for count, color in (color_data.split() for color_data in game.split(','))):
+        if all(count <= THRESHOLDS[color] for count, color in game_data):
             total_score += game_index + 1
     return total_score
 
 
 def part2():
     total = 0
-    for line in DATA:
+    for game_data in DATA:
         max_values = {'red': 0, 'green': 0, 'blue': 0}
-        games = line.split(':')[1].split(';')
-        for game in games:
-            for color_info in game.split(','):
-                count, color = map(str.strip, color_info.split())
-                count = int(count)
-                max_values[color] = max(max_values[color], count)
-        total += max_values['red'] * max_values['green'] * max_values['blue']
-
+        for count, color in game_data:
+            max_values[color] = max(max_values[color], count)
+        total += reduce(lambda x, y: x * y, max_values.values(), 1)
     return total
 
 
